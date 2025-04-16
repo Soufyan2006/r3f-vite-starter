9a842ed22d7b85035ef020ee30af0dbb3dc5bec6
Soufyan2006 <113595059+Soufyan2006@users.noreply.github.com>
2025-04-16 14:35:43 +0200

    ja

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,46 @@
-import { OrbitControls } from "@react-three/drei";
+import { ContactShadows, Environment ,OrbitControls, useCursor } from "@react-three/drei";
 
+import { AnimatedWoman } from "./AnimatedWoman";
+import { charactersAtom, socket } from "./SocketManager";
+import { useAtom } from "jotai";
+import { useState } from "react";
+import * as THREE from "three";
 export const Experience = () => {
+   const [characters] = useAtom(charactersAtom);
+  const [onFloor, setOnFloor] = useState(false);
+  useCursor(onFloor);
+
+
   return (
     <>
+
+  <Environment preset="sunset" />
+  <ambientLight intensity={0.3} />
+      <ContactShadows blur={2}/>
       <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
+      <mesh rotation-x={-Math.PI / 2} position-y={0.001} onClick={(e) => socket.emit("move", [e.point.x, 0, e.point.z])}
+        onPointerEnter={() => setOnFloor(true)}
+        onPointerLeave={() => setOnFloor(false)}>
+      
+        <planeGeometry args={[10,10]} />
+        <meshStandardMaterial color="red" />
       </mesh>
+      {
+        characters.map((character) =>(
+          <AnimatedWoman key={character.id} 
+          position={
+            new THREE.Vector3(
+              character.position[0],
+              character.position[1],
+              character.position[2]
+            )
+          }
+          hairColor={character.hairColor} 
+          topColor={character.topColor} 
+          bottomColor={character.bottomColor} />
+        ))
+      }
+
     </>
   );
 };
